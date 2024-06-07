671a206f043d0273a7d17346d2ea6fab5290d1e9
Ramesh <rameshpilli1428@gmail.com>
2024-06-07 16:11:58 +0530

    footers added to the admin panel

--- admin/public/newBlog.php
+++ admin/public/newBlog.php
@@ -147,7 +147,11 @@
                 <footer class="sticky-footer bg-white">
                     <div class="container my-auto">
                         <div class="copyright text-center my-auto">
-                            <span>Copyright &copy; Your Website 2021</span>
+                        <div class="footer-widget__copyright">
+              <p class="mini_text" style="color:black"> ©2024 Tripura-Mind-Care . All Rights Reserved. Designed &
+                Developed by <a href="https://bhavicreations.com/" target="_blank" style="text-decoration: none;color:black">Bhavi
+                  Creations</a></p>
+            </div>
                         </div>
                     </div>
                 </footer>
